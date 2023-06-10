a409c5919df3040039de583e6708ee8524a33172
Dheeraj Shrivastav <dheerajshrivastav08@gmail.com>
2023-06-10 14:03:33 +0530

    changes in next.config.js file

--- nft-wek4/next.config.js
+++ nft-wek4/next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  basePath: '/Fetching-NFT', // Replace with your repository name
+  assetPrefix: '/Fetching-NFT/', // Replace with your repository name
 }
